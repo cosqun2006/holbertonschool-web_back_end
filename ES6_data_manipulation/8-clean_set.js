f10d432bc7f969c59bc2e76bc0149dd7a88ee2d8
cosqun2006 <cosqunibrahimov500@gmail.com>
2026-08-02 23:47:39 +0400

    8. Clean set

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,19 @@
+export default function cleanSet(set, startString) {
+  if (
+    !set ||
+    !(set instanceof Set) ||
+    typeof startString !== 'string' ||
+    startString.length === 0
+  ) {
+    return '';
+  }
+
+  const result = [];
+  for (const value of set) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      result.push(value.slice(startString.length));
+    }
+  }
+
+  return result.join('-');
+}
