ab49bc465c8e54beab738f4dfa4f861e1fb71f69
cosqun2006 <cosqunibrahimov500@gmail.com>
2026-08-02 21:37:33 +0400

    2. Filter

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -2,4 +2,5 @@ export default function getStudentsByLocation(students, city) {
   if (!Array.isArray(students)) {
     return [];
   }
+  return students.filter((student) => student.location === city);
 }
